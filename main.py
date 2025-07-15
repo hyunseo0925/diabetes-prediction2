8c0270c1ba85aa7d1aafa8569abc5a8eac14790c
hyunseo0925 <hyde.park0925@gmail.com>
2025-07-15 09:33:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import plotly.express as px
 
-# 파일 경로 - 필요 시 수정
-csv_path = "diabetes_prediction_dataset - diabetes_prediction_dataset.csv"
+# 데이터 불러오기
+df = pd.read_csv("diabetes_prediction_dataset - diabetes_prediction_dataset.csv")
 
-# CSV 읽기
-df = pd.read_csv(csv_path)
-
-# 나이 그룹 나누기
+# 나이 구간 설정
 df['age_group'] = pd.cut(df['age'], bins=[0, 20, 30, 40, 50, 60, 70, 120],
                          labels=['10대 이하', '20대', '30대', '40대', '50대', '60대', '70대 이상'])
 
-# 유병률 계산
+# 연령대별 당뇨병 발병률(%) 계산
 age_diabetes_rate = df.groupby('age_group')['diabetes'].mean().reset_index()
 age_diabetes_rate['diabetes'] *= 100
 
-# 그래프 시각화
+# 그래프 그리기
 fig = px.bar(age_diabetes_rate,
              x='age_group',
              y='diabetes',
@@ -24,5 +21,4 @@ fig = px.bar(age_diabetes_rate,
              color='diabetes',
              color_continuous_scale='reds')
 
-# HTML 저장
-fig.write_html("diabetes_age_plot.html")
+fig.show()
